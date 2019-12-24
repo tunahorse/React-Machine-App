0bd1ba23e2d2b60403c189b543ec6c83d0ab2432
myusername <fabiananguiano@gmai.com>
2019-12-24 14:44:02 -0600

    cloud mongodb test hroku

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 
             <Navbar.Brand>
               <Link to={"/Student-list"} className="nav-link">
-                React Machine Down App
+                React Machine Down App Test 
               </Link>
             </Navbar.Brand>
 
