e3500827d324bcb18bdd1d56f1c7144f2616beec
myusername <fabiananguiano@gmai.com>
2019-12-26 13:35:29 -0600

    security updates

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db:'mongodb+srv://fabian:College1@cluster0-9dcpm.mongodb.net/test?retryWrites=true&w=majority'
+  db:'mongodb://localhost:27017/reactdb'
 
 
 
@@ -7,18 +7,8 @@ module.exports = {
 
 // test test test 
 
-
-
-
-// 'mongodb://localhost:27017/reactdb'
-
-
 };
 
 
 
-module.exports = {
-  MongoURI:
-    `mongodb://fishj123:College1}@ds149885.mlab.com:49885/heroku_26921dvn`,
-    
-};
+
